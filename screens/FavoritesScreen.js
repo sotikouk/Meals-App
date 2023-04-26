3cbc3e3b17af934e03663a0f316ee61e79bbdb8e
sotikouk <sotikou@uth.gr>
2023-04-26 17:29:45 +0300

    favorites screen complete with redux

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,12 +1,21 @@
-import { FavoritesContext } from '../store/context/favorites-context'
+//import { FavoritesContext } from '../store/context/favorites-context'
+import { useSelector } from "react-redux";
 import MealsList from "../components/mealsList/MealsList";
-import { useContext } from "react";
+//import { useContext } from "react";
 import { MEALS } from '../data/dummy-data';
+import { View, Text } from 'react-native';
 
 function FavoritesScreen() {
-    const FavoriteMealsCtx = useContext(FavoritesContext);
+   // const FavoriteMealsCtx = useContext(FavoritesContext);
+   
+   const favoriteMealIds = useSelector( state => state.favoriteMeals.ids);
 
-    const favoriteMeals = MEALS.filter( meal => FavoriteMealsCtx.ids.includes(meal.id));
+    const favoriteMeals = MEALS.filter( meal => favoriteMealIds.includes(meal.id));
+
+    if (favoriteMeals.length === 0)
+        return (<View>
+            <Text>You have no favorite meals yet.</Text>
+        </View>);
 
    return ( <MealsList items={favoriteMeals} />);
 }
